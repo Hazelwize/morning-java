083bea6df465ac9e0fca8310f998db26037680a0
Micah Jackson <thehazelwize@gmail.com>
2023-02-22 19:15:38 -0500

    add solution 1 and solution 2

---
+++ coding-challenge/Solution1.java
@@ -0,0 +1,9 @@
+public class Solution1{
+    int a = 9;
+    public static int addNums(int a){
+        return a + 9;
+    }
+    public static void main(String[] args){
+        System.out.println(addNums(9));
+    }
+}
