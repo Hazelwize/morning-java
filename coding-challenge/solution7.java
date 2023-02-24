5775168bf774e534eebb4d5defbd1330e8dd7608
Micah Jackson <thehazelwize@gmail.com>
2023-02-24 16:37:40 -0500

    add solution 7

---
+++ coding-challenge/solution7.java
@@ -0,0 +1,7 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+public class solution7 {
+    public static String boolToWord(boolean b){
+        return b == false ? "No" : "Yes";
+    }
+}
