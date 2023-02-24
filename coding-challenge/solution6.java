c58096ba36cb912da511d0ea41cb203f2dc92315
Micah Jackson <thehazelwize@gmail.com>
2023-02-24 16:32:18 -0500

    add solution 6

---
+++ coding-challenge/solution6.java
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+public class solution6 {
+    public static String solution(String str) {
+      return new StringBuilder(str).reverse().toString();
+    }    
+}
